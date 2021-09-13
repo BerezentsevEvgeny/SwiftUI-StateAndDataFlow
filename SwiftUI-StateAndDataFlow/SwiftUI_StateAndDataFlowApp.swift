2999f5ac104310c3dd3499f9121afbee935c0d5d
Berezentsev Evgeny <jonn09@icloud.com>
2021-09-13 20:59:56 +0300

    Initial Commit

---
+++ SwiftUI-StateAndDataFlow/SwiftUI_StateAndDataFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_StateAndDataFlowApp.swift
+//  SwiftUI-StateAndDataFlow
+//
+//  Created by Евгений Березенцев on 13.09.2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_StateAndDataFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
